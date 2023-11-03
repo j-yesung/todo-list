0a911fef20c28a7b237ace1eb243f116be969dc0
j-yesung <hisung-ah@kakao.com>
2023-11-03 16:37:44 +0900

    form.jsx code refactoring

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,18 @@
 import React, { useState } from 'react';
 import styles from '../styles/Main.module.css';
 
-function TodoItem({ todos, handleClickToggleTodo, handleClickRemoveTodo }) {
-  let buttonStyleClass = null;
-  todos.filter(todo =>
-    todo.isDone === false
-      ? (buttonStyleClass = styles.todoSuccessButton)
-      : (buttonStyleClass = styles.todoCancelButton),
-  );
-
-  return todos.map(todo => (
+function TodoItem(props) {
+  return props.todos.map(todo => (
     <div key={todo.id} className={styles.todoList}>
       <h3>{todo.title}</h3>
       <p>{todo.content}</p>
-      <button className={buttonStyleClass} onClick={() => handleClickToggleTodo(todo.id)}>
+      <button
+        className={todo.isDone ? styles.todoCancelButton : styles.todoSuccessButton}
+        onClick={() => props.handleClickToggleTodo(todo.id)}
+      >
         {todo.isDone ? '취소' : '완료'}
       </button>
-      <button className={styles.todoRemoveButton} onClick={() => handleClickRemoveTodo(todo.id)}>
+      <button className={styles.todoRemoveButton} onClick={() => props.handleClickRemoveTodo(todo.id)}>
         삭제
       </button>
     </div>
@@ -27,14 +23,17 @@ function Form() {
   const [todo, setTodo] = useState([]); // Todo 리스트 상태 관리
   const [title, setTitle] = useState(''); // 제목 입력 값 상태 관리
   const [content, setContent] = useState(''); // 내용 입력 값 상태 관리
+
   const workingTodos = todo.filter(todo => !todo.isDone);
+  console.log('workingTodos: ', workingTodos);
   const doneTodos = todo.filter(todo => todo.isDone);
+  console.log('doneTodos: ', doneTodos);
 
   // TODO 추가
   const handleClickAddTodo = () => {
-    if (title === '' && content === '') return alert('제목과 내용을 입력해 주세요.');
+    if (title.trim() === '' || content.trim() === '') return alert('제목과 내용을 입력해 주세요.');
     const newTodo = {
-      id: todo.length + 1,
+      id: Date.now(), // 고유 값
       title: title,
       content: content,
       isDone: false,
@@ -46,21 +45,16 @@ function Form() {
     setContent('');
   };
 
-  const handleTitleChange = e => setTitle(e.target.value);
-  const handleContentChange = e => setContent(e.target.value);
+  const handleChangeInput = e => (e.target.name === 'title' ? setTitle(e.target.value) : setContent(e.target.value));
   const handleClickRemoveTodo = id => setTodo(prevTodos => prevTodos.filter(todo => todo.id !== id));
   const handleClickToggleTodo = id =>
     setTodo(prevTodos => prevTodos.map(todo => (todo.id === id ? { ...todo, isDone: !todo.isDone } : todo)));
 
   return (
     <div>
-      <div
-        style={{
-          marginTop: '30px',
-        }}
-      >
-        <input type="text" placeholder="제목" value={title} onChange={handleTitleChange} />
-        <input type="text" placeholder="내용" value={content} onChange={handleContentChange} />
+      <div className={styles.todoContent}>
+        <input type="text" name="title" placeholder="제목" value={title} onChange={handleChangeInput} />
+        <input type="text" name="content" placeholder="내용" value={content} onChange={handleChangeInput} />
         <button className={styles.addButton} onClick={handleClickAddTodo}>
           TODO 추가
         </button>
