3e142abdf6090a73935fb7bd89eba31625cb235b
j-yesung <hisung-ah@kakao.com>
2023-11-16 16:03:25 +0900

    add dark mode (from. styled components)

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import styles from '../styles/Main.module.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from '../theme/theme';
+import { useState } from 'react';
+import { GlobalStyle } from '../theme/GlobalStyle';
+
+const Box = styled.div`
+  background: ${({ theme }) => theme.bgColor};
+  color: ${({ theme }) => theme.textColor};
+  border: 2px solid ${({ theme }) => theme.textColor};
+`;
 
 function Layout(props) {
+  const [mode, setMode] = useState(lightTheme);
+
+  const toggleTheme = () => {
+    if (mode === lightTheme) {
+      setMode(darkTheme);
+    } else {
+      setMode(lightTheme);
+    }
+  };
+
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        My Todo List
-        <div>React</div>
-      </header>
-      {props.children}
-    </div>
+    <ThemeProvider theme={mode}>
+      <GlobalStyle />
+      <Box className={styles.container}>
+        <header className={styles.heaer}>
+          <button onClick={toggleTheme}>모드 변경</button>
+          My Todo List
+          <div>React</div>
+        </header>
+        {props.children}
+      </Box>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/theme.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  bgColor: 'white',
+  textColor: 'black',
+};
+export const darkTheme = {
+  bgColor: 'black',
+  textColor: 'white',
+};
+
+export const theme = {
+  lightTheme,
+  darkTheme,
+};
